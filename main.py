6b5e1827138f9ddef6ffbea032e5b973a8a14d64
Nikolai <kolamba.r@gmail.com>
2024-01-27 14:50:52 +0300

    added handling of errors related to incorrect link spelling and inability to connect to the database

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import datetime
+from typing import Union
+
+from pymongo.errors import ServerSelectionTimeoutError
 
 from settings import logging
 from motor.motor_asyncio import AsyncIOMotorClient
 from starlette.requests import Request
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException
 from fastapi.responses import RedirectResponse
 
 from mongo_client import client
@@ -25,21 +28,20 @@ async def create_short_link(link: dict, request: Request) -> Response:
     )
     # Получаем ссылку
     link = link['link']
+    try:
+        connection_to_mongo: AsyncIOMotorClient = request.app.state.mongo_client["short_link_bd"]  # подключение к бд
+        response = await record_link(connection_to_mongo, link)
 
-    connection_to_mongo: AsyncIOMotorClient = request.app.state.mongo_client["short_link_bd"]  # подключение к бд
-    response = await record_link(connection_to_mongo, link)
-
-    return response
+        return response
+    except ServerSelectionTimeoutError as e:
+        # Логируем ошибку
+        logging.error(f"Ошибка подключения к базе данных: {e}")
+        # Возвращаем ответ с кодом состояния 500
+        return Response(status_code=500, content='Не удалось подключиться к базе данных')
 
 
 @app.get("/go/{short_link}")
-async def go_to_short_link(short_link: str, request: Request) -> RedirectResponse:
-    connection_to_mongo: AsyncIOMotorClient = request.app.state.mongo_client["short_link_bd"]  # подключение к бд
-
-    # Получаем из бд нужную ссылку и редиректим на нее
-    necessary_dict = await connection_to_mongo.records.find_one({"short_link": short_link})
-    link = necessary_dict['link']
-
+async def go_to_short_link(short_link: str, request: Request):
     # Логируем запрос
     logging.info(
         f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - "
@@ -47,22 +49,31 @@ async def go_to_short_link(short_link: str, request: Request) -> RedirectRespons
         f"метод запроса: {request.method}, "
         f"запрашиваемый путь: {request.url}"
     )
+    try:
+        connection_to_mongo: AsyncIOMotorClient = request.app.state.mongo_client["short_link_bd"]  # подключение к бд
 
-    # Создаем объект Response
-    response = RedirectResponse(link, status_code=301)
-    # Логируем код ответа
-    logging.info(f"Код ответа: {response.status_code}")
+        # Получаем из бд нужную ссылку и редиректим на нее
+        necessary_dict = await connection_to_mongo.records.find_one({"short_link": short_link})
+        if not necessary_dict:
+            raise HTTPException(status_code=404, detail="Ссылка не найдена")
+        link = necessary_dict['link']
 
-    return response
 
+
+        # Создаем объект Response
+        response = RedirectResponse(link, status_code=301)
+        # Логируем код ответа
+        logging.info(f"Код ответа: {response.status_code}")
+
+        return response
+    except ServerSelectionTimeoutError as e:
+        # Логируем ошибку
+        logging.error(f"Ошибка подключения к базе данных: {e}")
+        # Возвращаем ответ с кодом состояния 500
+        return Response(status_code=500, content='Не удалось подключиться к базе данных')
 
 @app.delete("/del/")
-async def go_to_short_link(short_link: dict, request: Request) -> Response:
-    connection_to_mongo: AsyncIOMotorClient = request.app.state.mongo_client["short_link_bd"]  # подключение к бд
-    short_code = get_short_code(short_link)
-    # Удаляем из бд нужную ссылку
-    connection_to_mongo.records.delete_one({"short_link": short_code})
-
+async def delete_short_link(short_link: dict, request: Request) -> Response:
     # Логируем запрос
     logging.info(
         f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - "
@@ -70,13 +81,25 @@ async def go_to_short_link(short_link: dict, request: Request) -> Response:
         f"метод запроса: {request.method}, "
         f"запрашиваемый путь: {request.url}"
     )
+    try:
+        connection_to_mongo: AsyncIOMotorClient = request.app.state.mongo_client["short_link_bd"]  # подключение к бд
+        short_code = get_short_code(short_link)
+        if await connection_to_mongo.records.find_one({"short_link": short_code}):
+            # Удаляем из бд нужную ссылку
+            connection_to_mongo.records.delete_one({"short_link": short_code})
+            # Создаем объект Response
+            response = Response(status_code=200, content='Данные с этой короткой ссылкой удалены')
+            # Логируем код ответа
+            logging.info(f"Код ответа: {response.status_code}")
+            return response
+        else:
+            raise HTTPException(status_code=404, detail="Ссылка не найдена")
+    except ServerSelectionTimeoutError as e:
+        # Логируем ошибку
+        logging.error(f"Ошибка подключения к базе данных: {e}")
+        # Возвращаем ответ с кодом состояния 500
+        return Response(status_code=500, content='Не удалось подключиться к базе данных')
 
-    # Создаем объект Response
-    response = Response(status_code=200, content='Данные с этой короткой ссылкой удалены')
-    # Логируем код ответа
-    logging.info(f"Код ответа: {response.status_code}")
-
-    return response
 
 if __name__ == "__main__":
     import uvicorn
